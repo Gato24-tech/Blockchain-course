9b6155958b9eca91742a228237364740b95c6e5d
Ivan <Ivan.queralt@gmail.com>
2025-01-08 18:22:59 +0100

    interaccion con AdvancedContract completada

--- interact-advanced.js
+++ interact-advanced.js
@@ -2,7 +2,7 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-    const AdvancedContract = await ethers.getContractAt("AdvancedContract", "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512");
+    const AdvancedContract = await ethers.getContractAt("AdvancedContract", "0x5FbDB2315678afecb367f032d93F642f64180aa3");
     await AdvancedContract.getValue(); // Debería devolver un valor predeterminado si está funcionando correctamente.
     
 
