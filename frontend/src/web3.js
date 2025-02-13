39bb4b8ab53a961d7f2ff3e8559a0d28dab70e21
Gato24-tech <ivan.queralt@gmail.com>
2025-02-13 08:21:03 +0100

    web3 corregida temporalmente

--- frontend/src/web3.js
+++ frontend/src/web3.js
@@ -1,29 +1,59 @@
 import { ethers } from "ethers";
-import deployments from "./deployments.json";
+
+
+async function _getDeployments() { 
+ try {
+ const response = await fetch(`/deployments.json`);
+ if (!response.ok) {
+   throw new Error(`HTTP error! Status: ${response.status}`);
+}
+return await response.json();
+} catch (error) {
+ console.error("Error obtenido deployments:",error);
+ return null;
+}
+}
+
 
 const connectWallet = async () => {
-  if (!window.ethereum) {
-    alert("MetaMask no está instalado");
-    return;
-  }
+ if (!window.ethereum) {
+   alert("MetaMask no está instalado");
+   return null;
+ }
 
-  const provider = new ethers.BrowserProvider(window.ethereum);
-  const signer = await provider.getSigner();
-  console.log("Conectado con:", await signer.getAddress());
-  return signer;
+
+ try {
+   // Solicitar acceso a MetaMask
+   const provider = new ethers.BrowserProvider(window.ethereum);
+   const signer = await provider.getSigner();
+   const account = await signer.getAddress();
+
+
+   console.log("Cuenta conectada:", account);
+   return { provider, signer, account };
+ } catch (error) {
+   console.error("Error conectando con MetaMask:", error);
+   return null;
+ }
 };
+
 
 const getContract = async () => {
-  const signer = await connectWallet();
-  if (!signer) return null;
+ const { provider, signer } = await connectWallet();
+ if (!provider || !signer) return null;
 
-  const contract = new ethers.Contract(
-    deployments.contractAddress,
-    deployments.abi,
-    signer
-  );
 
-  return contract;
+const deployments = await _getDeployments();
+if (!deployments || !deployments.contractAddress || !deployments.abi) {
+ console.error("No se encontró la direccion o ABI del contrato en deployments.json");
+ return null;
+}
+
+
+const contract = new ethers.Contract(deployments.contractAddress, deployments.abi, signer);
+console.log("Contrato conectado:", contract);
+return contract;
 };
+
 
 export { connectWallet, getContract };
