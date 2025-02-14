10b5f96f5e524e4b341f57c50adcc6c37038e1f2
Gato24-tech <ivan.queralt@gmail.com>
2025-02-14 13:18:06 +0100

    Modificación de read-data

--- scripts/AdvancedContract/read-data.js
+++ scripts/AdvancedContract/read-data.js
@@ -4,16 +4,13 @@ import process from "process";
 import path from "path";
 import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const deploymentsPath = path.join(__dirname, "../../frontend/public/deployments.json");
+
 async function main() {
     const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
 
-    // Definir __dirname
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
-
-    // Cargar deployments.json para obtener la dirección y ABI del contrato
-       const deploymentsPath = path.join(__dirname, "../../frontend/public/deployments.json");
-    
     if (!fs.existsSync(deploymentsPath)) {
         console.error("Error: No se encontró deployments.json.");
         process.exit(1);
@@ -21,13 +18,17 @@ async function main() {
 
     const deployments = JSON.parse(fs.readFileSync(deploymentsPath, "utf-8"));
 
-    if (!deployments.contractAddress || !deployments.abi) {
+    // Extraer información desde localhost
+    if (!deployments.localhost || !deployments.localhost.address || !deployments.localhost.abi) {
         console.error("Error: No se encontró la dirección o el ABI en deployments.json.");
         process.exit(1);
     }
 
+    const contractAddress = deployments.localhost.address;
+    const contractABI = deployments.localhost.abi;
+
     // Crear la instancia del contrato
-    const contract = new ethers.Contract(deployments.contractAddress, deployments.abi, provider);
+    const contract = new ethers.Contract(contractAddress, contractABI, provider);
 
     // Llamar a la función del contrato
     const value = await contract.getValue();
